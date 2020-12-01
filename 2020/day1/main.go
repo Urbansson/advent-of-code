87acd3069a8e1da839f7990c30c099d710a71549
Theodor Brandt <theodor.brandt@svt.se>
2020-12-01 17:28:57 +0100

    2020 solution day1 part 2

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -9,12 +9,21 @@ import (
 func main() {
 	s := aoc.ReadInput("./input.txt")
 	is := aoc.ExtractInts(s)
-	if s1, s2, ok := twoSum(is, 2020); ok {
-		fmt.Printf("%d+%d=%d\n", s1, s2, s1+s2)
-		fmt.Println(s1 * s2)
+	if s1, s2, s3, ok := threeSum(is, 2020); ok {
+		fmt.Printf("%d+%d+%d=%d\n", s1, s2, s3, s1+s2+s3)
+		fmt.Println(s1 * s2 * s3)
 	} else {
 		fmt.Println("target not found")
 	}
+}
+
+func threeSum(nums []int, target int) (int, int, int, bool) {
+	for i, k := range nums {
+		if s1, s2, ok := twoSum(nums[i:], target - k); ok {
+			return k, s1, s2, true
+		}
+	}
+	return 0, 0, 0, false
 }
 
 func twoSum(nums []int, target int) (int, int, bool) {
