bf023701429af93921da6db628ba64ca5263b3a7
Theodor Brandt <theodor.brandt@svt.se>
2020-12-11 15:49:25 +0100

    Solution for day8 part 2

--- 2020/day8/main.go
+++ 2020/day8/main.go
@@ -10,12 +10,29 @@ import (
 func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
-	cache := make([]bool, len(lines))
+	for i, l := range lines {
+		cache := lines[i]
+		if strings.HasPrefix(l, "jmp") {
+			lines[i] = strings.Replace(l, "jmp", "nop", 1)
+		}
+		if strings.HasPrefix(l, "nop") {
+			lines[i] = strings.Replace(l, "nop", "jmp", 1)
+		}
+		if acc, ok := run(lines); ok {
+			fmt.Println(acc)
+			return
+		}
+		lines[i] = cache
+	}
+}
+
+func run(input []string) (int, bool) {
+	cache := make([]bool, len(input))
 	acc := 0
-	for i := 0; i < len(lines); {
-		d := strings.Split(lines[i], " ")
+	for i := 0; i < len(input); {
+		d := strings.Split(input[i], " ")
 		if cache[i] {
-			break
+			return acc, false
 		}
 		cache[i] = true
 		switch d[0] {
@@ -28,5 +45,5 @@ func main() {
 			i++
 		}
 	}
-	fmt.Println(acc)
+	return acc, true
 }
