0fc381e3cde035c54963524d408f993c607d1228
Theodor Brandt <3965059+Urbansson@users.noreply.github.com>
2020-12-17 15:21:44 +0100

    Solution for day10 part 1

--- 2020/day10/main.go
+++ 2020/day10/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+
+	"github.com/Urbansson/advent-of-code/2020/pkg/aoc"
+)
+
 func main() {
-	$END$
+	data := aoc.ReadStdin()
+	a := aoc.ExtractInts(data)
+	sort.Ints(a)
+	a = append(a, a[len(a)-1]+3)
+	fmt.Println(findChain(a))
+}
+
+func findChain(a []int) int {
+	differences := map[int]int{
+		1: 0,
+		2: 0,
+		3: 0,
+	}
+	previous := 0
+	for _, current := range a {
+		difference := current - previous
+		differences[difference]++
+		previous = current
+	}
+	return differences[1] * differences[3]
 }
