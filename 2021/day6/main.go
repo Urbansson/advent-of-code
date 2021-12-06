8d183af46b05b289a6ae66850f0d2e61539bdafd
Theodor Brandt <theodor.brandt@volumental.com>
2021-12-06 10:45:43 +0100

    Solution for day 6 part 2.

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -9,31 +9,39 @@ import (
 func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
-	dig := aoc.ExtractInts(lines[0])
+	fishes := aoc.ExtractInts(lines[0])
 
-	fishes := make([]lanternfish, len(dig))
-	for i, a := range dig {
-		fishes[i] = lanternfish{
-			age: a,
-		}
+	var (
+		M         = 10
+		dueDates  = make([]int, M)
+		dueDate   int
+		fish      = 0
+		today     int
+		newFish   int
+		newFishDD int
+		oldFishDD int
+	)
+
+	for _, ini := range fishes {
+		dueDate = ini + 1
+		dueDates[dueDate]++
+		fish++
 	}
 
-	for i := 0; i < 80; i++ {
-		newFishes := []lanternfish{}
-		for i := range fishes {
-			fishes[i].age = fishes[i].age - 1
-			if fishes[i].age < 0 {
-				newFishes = append(newFishes, lanternfish{
-					age: 8,
-				})
-				fishes[i].age = 6
-			}
-		}
-		fishes = append(fishes, newFishes...)
-	}
-	fmt.Println(len(fishes))
-}
+	// simulate births for N days
+	for day := 1; day <= 256; day++ {
+		today = day % M
+		// birth new fish
+		newFish = dueDates[today]
+		fish += newFish
 
-type lanternfish struct {
-	age int
+		// update due dates
+		newFishDD = (today + 9) % M
+		oldFishDD = (today + 7) % M
+		dueDates[newFishDD] += newFish
+		dueDates[oldFishDD] += newFish
+		dueDates[today] -= newFish
+	}
+
+	fmt.Println(fish)
 }
