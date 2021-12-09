9e765a3cb84787e033b77d9e037c4095f03df800
Theodor Brandt <theodor.brandt@volumental.com>
2021-12-09 22:15:32 +0100

    Solution for day 9 part 2.

--- 2021/day9/main.go
+++ 2021/day9/main.go
@@ -2,42 +2,74 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/Urbansson/advent-of-code/pkg/aoc"
 )
+
+type pos struct {
+	x int
+	y int
+}
+
+var adjacent = []pos{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 
 func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
 
-	cave := make([][]int, len(lines))
-	for i, l := range lines {
-		r := aoc.DigitList(l)
-		cave[i] = r
+	cave := map[pos]int{}
+	for x, l := range lines {
+		for y, h := range aoc.DigitList(l) {
+			cave[pos{x: x, y: y}] = h
+		}
 	}
-
 	res := 0
-	for x := range cave {
-		for y := range cave[x] {
-			height := cave[x][y]
-			var above, left, under, right = 9, 9, 9, 9
-			if x-1 >= 0 {
-				above = cave[x-1][y]
+	var lowpoints []pos
+	for p, height := range cave {
+		low := 9
+		for _, a := range adjacent {
+			if ah, ok := cave[pos{
+				x: p.x + a.x,
+				y: p.y + a.y,
+			}]; ok {
+				if ah < low {
+					low = ah
+				}
 			}
-			if y-1 >= 0 {
-				left = cave[x][y-1]
-			}
-			if x+1 < len(cave) {
-				under = cave[x+1][y]
-			}
-			if y+1 < len(cave[x]) {
-				right = cave[x][y+1]
-			}
-			if height < above && height < left && height < under && height < right {
-				fmt.Println(x, y, height)
-				res += height + 1
-			}
+		}
+		if height < low {
+			res += height + 1
+			lowpoints = append(lowpoints, p)
 		}
 	}
 	fmt.Println(res)
+	var sums []int
+	for _, lowpoint := range lowpoints {
+		s := larger(cave, lowpoint, lowpoint, make(map[pos]bool))
+		sums = append(sums, s)
+	}
+	sort.Ints(sums)
+	fmt.Println(sums[len(sums)-3] * sums[len(sums)-2] * sums[len(sums)-1])
+
+}
+
+func larger(grid map[pos]int, start pos, root pos, seen map[pos]bool) (sum int) {
+	sum += 1
+	for _, a := range adjacent {
+		newP := pos{
+			x: start.x + a.x,
+			y: start.y + a.y,
+		}
+		if _, ok := seen[newP]; ok {
+			continue
+		}
+		if vv, ok := grid[newP]; ok {
+			if vv > grid[root] && vv != 9 {
+				seen[newP] = true
+				sum += larger(grid, newP, root, seen)
+			}
+		}
+	}
+	return
 }
