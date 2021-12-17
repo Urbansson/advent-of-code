467427c39386109adfe1797ece213ba346aac5c3
Theodor Brandt <theodor.brandt@volumental.com>
2021-12-17 10:36:25 +0100

    Solution for day 16 part 2.

--- 2021/day16/main.go
+++ 2021/day16/main.go
@@ -13,40 +13,43 @@ import (
 func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
-
-	_, value := parse(hexToBinary(lines[0]), 0)
-	fmt.Println("res", value)
+	fmt.Println(Parse(hexToBinary(lines[0])))
 }
 
-func parse(data string, offset int) (int, int) {
-	fmt.Printf("parsing data %d from offset %d\n", len(data), offset)
-	fmt.Println(data[offset:])
+func Parse(data string) (int, int) {
+	_, versions, value := parse(data, 0)
+	return versions, value
+}
+
+// parse returns offset, version sum, value
+func parse(data string, offset int) (int, int, int) {
 	v := binaryToDecimal(data[offset+0 : offset+3])
 	pkg := binaryToDecimal(data[offset+3 : offset+3+3])
-
-	fmt.Println("version", v, "package", pkg)
-	// literal value cant have any nested packets just return version and new offset.
+	// literal value cant have any nested packets just return new offset, version and value.
 	if pkg == 4 {
 		offset += 6
+		value := ""
 		for {
 			last := data[offset] == '0'
+			value += data[offset+1 : offset+5]
 			offset += 5
 			if last {
 				break
 			}
 		}
-		return offset, v
+		return offset, v, binaryToDecimal(value)
 	}
+	values := []int{}
 	lt := data[offset+6]
 	if lt == '0' {
 		l := binaryToDecimal(data[offset+7 : offset+7+15])
 		offset = offset + 7 + 15
 		end := offset + int(l)
 		for {
-			nextOffset, addV := parse(data, offset)
+			nextOffset, addV, value := parse(data, offset)
 			v += addV
 			offset = nextOffset
-			fmt.Println(end, nextOffset, end)
+			values = append(values, value)
 			if nextOffset >= end {
 				break
 			}
@@ -55,13 +58,45 @@ func parse(data string, offset int) (int, int) {
 		l := binaryToDecimal(data[offset+7 : offset+7+11])
 		offset = offset + 7 + 11
 		for i := 0; i < l; i++ {
-			fmt.Println(offset)
-			nextOffset, addV := parse(data, offset)
+			nextOffset, addV, value := parse(data, offset)
 			v += addV
 			offset = nextOffset
+			values = append(values, value)
 		}
 	}
-	return offset, v
+
+	res := 0
+	switch pkg {
+	case 0:
+		for _, v := range values {
+			res += v
+		}
+	case 1:
+		for _, v := range values {
+			res *= v
+		}
+	case 2:
+		for _, v := range values {
+			res = aoc.Min(res, v)
+		}
+	case 3:
+		for _, v := range values {
+			res = aoc.Max(res, v)
+		}
+	case 5:
+		if values[0] > values[1] {
+			res = 1
+		}
+	case 6:
+		if values[0] < values[1] {
+			res = 1
+		}
+	case 7:
+		if values[0] == values[1] {
+			res = 1
+		}
+	}
+	return offset, v, res
 }
 
 func hexToBinary(s string) string {
