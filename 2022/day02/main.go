afdc43d4b5c6543a60649ec0043b98270e12b050
Theodor Brandt <teddy@brandts.se>
2022-12-02 10:36:46 +0100

    Solution for 2022/02 part 2.

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -22,13 +22,27 @@ var lookup = map[string]int{
 	"C Z": 3 + 3, // DRAW
 }
 
+var lookup2 = map[string]int{
+	"A X": 0 + 3, // LOSE
+	"A Y": 3 + 1, // DRAW
+	"A Z": 6 + 2, // WIN
+	"B X": 0 + 1, // LOSE
+	"B Y": 3 + 2, // DRAW
+	"B Z": 6 + 3, // WIN
+	"C X": 0 + 2, // LOSE
+	"C Y": 3 + 3, // DRAW
+	"C Z": 6 + 1, // WIN
+}
+
 func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
 
-	sum := 0
+	sum1 := 0
+	sum2 := 0
 	for _, l := range lines {
-		sum += lookup[l]
+		sum1 += lookup[l]
+		sum2 += lookup2[l]
 	}
-	fmt.Println(sum)
+	fmt.Println(sum1, sum2)
 }
