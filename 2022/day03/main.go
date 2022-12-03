fc310d22786a814af9f73d941a5777f58486206c
Theodor Brandt <teddy@brandts.se>
2022-12-03 18:04:49 +0100

    Solution for 2022/03 part 2.

--- 2022/day03/main.go
+++ 2022/day03/main.go
@@ -14,7 +14,6 @@ func main() {
 	for _, r := range lines {
 		set := map[rune]struct{}{}
 		first, last := r[:len(r)/2], r[len(r)/2:]
-
 		for _, char := range first {
 			set[char] = struct{}{}
 		}
@@ -25,7 +24,33 @@ func main() {
 			}
 		}
 	}
-	fmt.Println(total)
+
+	fmt.Println(total, findBadge(lines))
+}
+
+func findBadge(lines []string) int {
+	total := 0
+	for i := 0; i < len(lines); i += 3 {
+		a, b, c := lines[i], lines[i+1], lines[i+2]
+		s1 := map[rune]struct{}{}
+		s2 := map[rune]struct{}{}
+		for _, char := range a {
+			s1[char] = struct{}{}
+		}
+		for _, char := range b {
+			s2[char] = struct{}{}
+		}
+		for _, char := range c {
+			_, ok1 := s1[char]
+			_, ok2 := s2[char]
+			if ok1 && ok2 {
+				total += toPrio(char)
+				break
+			}
+		}
+	}
+
+	return total
 }
 
 func toPrio(r rune) int {
