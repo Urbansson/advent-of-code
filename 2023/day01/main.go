2a912477bc80cd45b26d2522b3e5d66aec298fc1
Theodor Brandt <teddy@brandts.se>
2023-12-01 10:37:14 +0100

    Solution for day1 2023 part 2

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode"
 
 	"github.com/Urbansson/advent-of-code/pkg/aoc"
@@ -12,10 +13,24 @@ func main() {
 	lines := aoc.ExtractLines(data)
 
 	sum := 0
+	// fix edgecase of oneight == 18, not 1ight.
+	r := strings.NewReplacer(
+		"one", "1e",
+		"two", "2o",
+		"three", "3e",
+		"four", "4r",
+		"five", "5e",
+		"six", "6x",
+		"seven", "7n",
+		"eight", "8t",
+		"nine", "9e",
+	)
 	for _, l := range lines {
+		// Run replacer twice to fix the updated values.
+		nl := r.Replace(r.Replace(l))
 		first := 0
 		last := 0
-		for _, c := range l {
+		for _, c := range nl {
 			if ok := unicode.IsDigit(c); ok {
 				if first == 0 {
 					first = int(c-'0') * 10
@@ -24,7 +39,6 @@ func main() {
 			}
 		}
 		sum += first + last
-
 	}
 
 	fmt.Println(sum)
