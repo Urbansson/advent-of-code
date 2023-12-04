85a8aebc9c9c32c4f939e5d1585640bb445a1f14
Theodor Brandt <teddy@brandts.se>
2023-12-04 16:19:37 +0100

    Solution for day2 2023 part 2

---
+++ 2023/day02/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/Urbansson/advent-of-code/pkg/aoc"
+)
+
+func main() {
+	data := aoc.ReadStdin()
+	lines := aoc.ExtractLines(data)
+
+	re := regexp.MustCompile(`(\d+) (\w+)`)
+
+	sum := 0
+
+	for i, l := range lines {
+		r := strings.Split(l, ":")[1]
+
+		lu := map[string]int{}
+
+		for _, m := range re.FindAllStringSubmatch(r, -1) {
+			n, _ := strconv.Atoi(m[1])
+			lu[m[2]] = aoc.Max(n, lu[m[2]])
+		}
+
+		if lu["red"] <= 12 && lu["green"] <= 13 && lu["blue"] <= 14 {
+			sum += i + 1
+		}
+	}
+
+	fmt.Println(sum)
+}
