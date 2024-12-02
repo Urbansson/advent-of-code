519eb3a0a8262f3087fc4de2920ea930099ffb79
Theodor Brandt <teddy@brandts.se>
2024-12-02 15:53:26 +0100

    more refactor

--- 2024/day02/main.go
+++ 2024/day02/main.go
@@ -16,44 +16,32 @@ func main() {
 		il := aoc.ExtractInts(l)
 
 		if safeCheck((il)) {
-			//fmt.Println("safe", il)
 			sum++
-
 		}
-		fmt.Println("---")
 	}
-
 	fmt.Println("sum", sum)
 }
 
-func safeCheck(i []int) bool {
-	fmt.Println("testing", i)
-	dir := i[0] - i[len(i)-1]
-	p := i[0]
-	for _, c := range i[1:] {
-		fmt.Println("check if safe level", p, c)
-		fmt.Println("check step size", int(math.Abs(float64(p-c))), safeLavelChange(int(math.Abs(float64(p-c)))))
-		if !safeLavelChange(int(math.Abs(float64(p - c)))) {
-			fmt.Println("[Not Valid step size]")
+func safeCheck(input []int) bool {
+	isIncreasing := false
+	isDecreasing := false
+
+	prev := input[0]
+
+	for _, curr := range input[1:] {
+		slc := safeLavelChange(int(math.Abs(float64(prev - curr))))
+		if curr > prev {
+			isIncreasing = true
+		}
+		if curr < prev {
+			isDecreasing = true
+		}
+		prev = curr
+		// We cant be both increaseing and decrease
+		if !slc || (isIncreasing && isDecreasing) {
 			return false
 		}
-		// We expect the value to increase
-		if dir < 0 {
-			fmt.Println("value should increase", p, ">", c, p > c)
-			if p > c {
-				fmt.Println("[Wrong direction, should increase]")
-				return false
-			}
-		} else {
-			fmt.Println("value should decrease", p, "<", c, p < c)
-			if p < c {
-				fmt.Println("[Wrong direction, should decrease]")
-				return false
-			}
-		}
-		p = c
 	}
-	fmt.Println("Valid report")
 	return true
 }
 
