a4ae45fae4f0062a3430713d1917b684a2397cfb
Theodor Brandt <teddy@brandts.se>
2024-12-03 10:04:27 +0100

    Solution for day03 2025 part 2.

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -9,14 +9,25 @@ import (
 
 func main() {
 	data := aoc.ReadStdin()
-	r := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	r := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
 	ri := regexp.MustCompile(`\d{1,3}`)
 	sum := 0
+	do := true
 	for _, l := range r.FindAllString(data, -1) {
-		i := ri.FindAllString(l, -1)
-		v1 := aoc.Atoi(i[0])
-		v2 := aoc.Atoi(i[1])
-		sum += v1 * v2
+		if l == "do()" {
+			do = true
+			continue
+		}
+		if l == "don't()" {
+			do = false
+			continue
+		}
+		if do {
+			i := ri.FindAllString(l, -1)
+			v1 := aoc.Atoi(i[0])
+			v2 := aoc.Atoi(i[1])
+			sum += v1 * v2
+		}
 	}
 	fmt.Println(sum)
 }
