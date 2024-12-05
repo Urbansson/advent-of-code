1c5aef3751927624dd0c388a96bae84f5475693f
Theodor Brandt <teddy@brandts.se>
2024-12-05 13:46:26 +0100

    Solution for day05 2024 part 2.

--- 2024/day05/main.go
+++ 2024/day05/main.go
@@ -16,92 +16,49 @@ func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
 
-	// Contain what pages needs to be printed after the keyed page.
-	after := map[int][]int{}
-
-	// COntains what pages needs to be printed before the keyed page.
-	before := map[int][]int{}
-
 	rules := []rule{}
 
 	pi := 0
-
 	for i, l := range lines {
 		if l == "" {
 			pi = i + 1
 			break
 		}
 		s := strings.Split(l, "|")
-		b := aoc.Atoi(s[0])
-		a := aoc.Atoi(s[1])
-		after[b] = append(after[b], a)
-		before[a] = append(before[a], b)
-
-		rules = append(rules, rule{start: b, end: a})
-
+		rules = append(rules, rule{start: aoc.Atoi(s[0]), end: aoc.Atoi(s[1])})
 	}
 
 	updates := [][]int{}
 	for _, l := range lines[pi:] {
-		if l == "" {
-
-			break
-		}
 		updates = append(updates, aoc.ExtractInts(l))
-
 	}
-
-	for k, v := range after {
-		fmt.Println(k, ": ", v)
-	}
-	fmt.Println("---")
-	for k, v := range before {
-		fmt.Println(k, ": ", v)
-	}
-	fmt.Println("---")
-	for _, v := range updates {
-		fmt.Println(v)
-	}
-	fmt.Println("---")
 
 	sum := 0
 
 	for _, update := range updates {
-		valid := true
-		fmt.Println("Checking if update is valid,", update)
-
-		for _, r := range rules {
-
-			fmt.Println("check rule", r)
-
+		valid := false
+		for i := 0; i < len(rules); i++ {
+			r := rules[i]
 			si := contains(update, r.start)
-
 			ei := contains(update, r.end)
 
 			if si < 0 {
 				continue
 			}
-
 			if ei < 0 {
 				continue
 			}
 
-			fmt.Println(si, ei)
 			if si > ei {
-				fmt.Println("invalid")
-
-				valid = false
+				update[si], update[ei] = update[ei], update[si]
+				valid = true
+				i = 0
 			}
 		}
 
-		fmt.Println(valid, update)
 		if valid {
-
-			// fmt.Println("Middle valie", u[len(u)/2])
 			sum += update[len(update)/2]
 		}
-		fmt.Println("---")
-
 	}
 	fmt.Println(sum)
 }
