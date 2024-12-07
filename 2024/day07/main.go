1512902780f05f946f21e533f39ab779128465f7
Theodor Brandt <teddy@brandts.se>
2024-12-08 00:20:10 +0100

    Solution for day07 2024 part 1.

---
+++ 2024/day07/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Urbansson/advent-of-code/pkg/aoc"
+)
+
+func main() {
+	data := aoc.ReadStdin()
+	lines := aoc.ExtractLines(data)
+
+	sum := 0
+	for _, l := range lines {
+		r := strings.Split(l, ":")
+		res := aoc.Atoi(r[0])
+		nums := aoc.ExtractInts(r[1])
+
+		if calc(res, nums[0], nums[1:], fmt.Sprintf("%d", nums[0])) {
+			sum += res
+		}
+		//	fmt.Println("---")
+	}
+
+	fmt.Println(sum)
+}
+
+func calc(res, cur int, i []int, f string) bool {
+	if len(i) == 0 {
+		if cur == res {
+			fmt.Println(f)
+			return true
+		}
+		return false
+	}
+
+	next := i[0]
+	sum := false
+
+	if calc(res, cur+next, i[1:], fmt.Sprintf("%s+%d", f, next)) {
+		sum = true
+	}
+
+	if calc(res, cur*next, i[1:], fmt.Sprintf("%s*%d", f, next)) {
+		sum = true
+	}
+
+	return sum
+}
