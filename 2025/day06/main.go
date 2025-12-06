461215f485228fba86fdd436f39f437dcd71ea18
Theodor Brandt <me@brandts.dev>
2025-12-06 20:44:11 +0100

    Solution for day6 2025 part 2

--- 2025/day06/main.go
+++ 2025/day06/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/Urbansson/advent-of-code/pkg/aoc"
@@ -10,28 +12,48 @@ func main() {
 	data := aoc.ReadStdin()
 	lines := aoc.ExtractLines(data)
 
-	n := [][]int{}
-	for _, line := range lines[:len(lines)-1] {
-		il := aoc.IntList(line)
-		n = append(n, il)
-	}
 	ops := strings.Fields(lines[len(lines)-1])
 
-	ss := 0
-	for i := range n[0] {
-		op := ops[i]
-		sum := 0
-		for j := range n {
-			f := n[j][i]
-			if op == "*" {
-				if sum == 0 {
-					sum++
-				}
-				sum *= f
-			} else {
-				sum += f
-			}
-		}
-		ss += sum
+	// Find longest row
+	max := 0
+	for _, v := range lines {
+		max = aoc.Max(max, len(v))
 	}
+
+	// Make sure all rows are the same length by padding spaces to the end
+	for i, v := range lines {
+		if len(v) != max {
+			padding := max - len(v)
+			lines[i] = v + strings.Repeat(" ", padding)
+		}
+	}
+
+	n := 0
+	currentSum := 0
+	totalSum := 0
+	for i := 0; i < max; i++ {
+		op := ops[n]
+		rn := ""
+		for _, v := range lines[:len(lines)-1] {
+			rn += string(v[i])
+		}
+		v, err := strconv.Atoi(strings.Trim(rn, " "))
+		// If we fail to parse its the devider of an string with only spaces.
+		// Reset and add to totalSum
+		if err != nil {
+			n++
+			totalSum += currentSum
+			currentSum = 0
+			continue
+		}
+		if op == "*" {
+			if currentSum == 0 {
+				currentSum++
+			}
+			currentSum *= v
+		} else {
+			currentSum += v
+		}
+	}
+	fmt.Println(totalSum + currentSum)
 }
